7accd44a8be9bfb4ffdc8806e037895e6272524f
deguchi <deguchitoexit@gmail.com>
2021-10-27 11:25:45 +0900

    update: add rollup plugin livereload, terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,15 @@ import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import serve from "rollup-plugin-serve";
 import replace from "@rollup/plugin-replace";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
   output: {
     file: "src/bundle.js",
     format: "esm",
+    compact: true,
   },
   plugins: [
     nodeResolve(),
@@ -25,5 +28,9 @@ export default {
       "process.env.NODE_ENV": JSON.stringify("production"),
       preventAssignment: true,
     }),
+    livereload({
+      watch: "src",
+    }),
+    terser(),
   ],
 };
