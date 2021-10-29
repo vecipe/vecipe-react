0e29ff09184486cfd3f63959b341491c928da5dd
deguchi <deguchitoexit@gmail.com>
2021-10-29 13:06:03 +0900

    update: テーマカラーを追加した

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,37 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
 
 const config = {
   initialColorMode: "light",
   useSystemColorMode: false,
 };
-const theme = extendTheme({ config });
+const colors = {
+  vecipe: {
+    orange: {
+      50: "#FFF7E6",
+      100: "#FFE7B8",
+      200: "#FED88A",
+      300: "#FEC95D",
+      400: "#FEBA2F",
+      500: "#FEAB01",
+      600: "#CB8901",
+      700: "#986601",
+      800: "#664400",
+      900: "#332200",
+    },
+    green: {
+      50: "#F5F7ED",
+      100: "#E3EACD",
+      200: "#D1DCAD",
+      300: "#BECE8D",
+      400: "#ACC06D",
+      500: "#9AB34C",
+      600: "#7B8F3D",
+      700: "#5C6B2E",
+      800: "#3E471F",
+      900: "#1F240F",
+    },
+  },
+};
+const theme = extendTheme({ config, colors });
 
 export default theme;
