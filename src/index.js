77bdd35c441afe70ccd7a2ea388c35fa88c4b3b0
deguchi <deguchitoexit@gmail.com>
2021-10-29 18:34:43 +0900

    fix: マージのコンフリクトを解決した

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/provider";
 import TagsBox from "./TagsBox/TagsBox.jsx";
 import theme from "./theme.js";
-//import "./global.css";
-
-import TagsBox from "./TagsBox/TagsBox.jsx";
 
 const element = document.getElementsByClassName("archive_filter_tag")[0];
 element.innerHTML = "";
