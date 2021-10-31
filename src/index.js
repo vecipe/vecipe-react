a8fb85821cd3f98705de9d00ea3c5731a12e66ba
viewpointics-tan <viewpointicstan@gmail.com>
2021-10-31 10:54:26 +0900

    update: ソーシャルウィジェット削除を実装した

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/provider";
 import TagsBox from "./TagsBox/TagsBox.jsx";
 import theme from "./theme.js";
 
+//絞り込み検索のUIを書き換える
 const element = document.getElementsByClassName("archive_filter_tag")[0];
 element.innerHTML = "";
 ReactDOM.render(
@@ -12,3 +13,15 @@ ReactDOM.render(
   </ChakraProvider>,
   element
 );
+
+//おすすめ店舗リストのソーシャルウィジェットを削除する
+const pathname = document.location.pathname;
+if (pathname === "/blog/保存リスト") {
+  [
+    ...document.getElementsByClassName(
+      "veu_socialSet veu_socialSet-position-after veu_contentAddSection"
+    ),
+  ].map((element) => {
+    element.remove();
+  });
+}
