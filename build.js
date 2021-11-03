bba87fb8b15d99fa86fd7ed16cb1eeb031aa43a1
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-03 09:44:23 +0900

    update: ビルド結果をscript.htmlにも出力するよう設定した

--- build.js
+++ build.js
@@ -1,7 +1,22 @@
-require("esbuild").build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  loader: { ".js": "jsx" },
-  outdir: "dist",
-  minify: true,
-});
+const fs = require("fs/promises");
+const { Buffer } = require("buffer");
+
+require("esbuild")
+  .build({
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    loader: { ".js": "jsx" },
+    outdir: "dist",
+    minify: true,
+  })
+  .then(async () => {
+    const buffer = await fs.readFile("./dist/index.js");
+    fs.writeFile(
+      "./dist/script.html",
+      Buffer.concat([
+        Buffer.from("<script>\n"),
+        buffer,
+        Buffer.from("\n</script>"),
+      ])
+    );
+  });
