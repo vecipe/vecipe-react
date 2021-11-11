2007f7aa3277c263be6b235eed2a340d5895a396
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-11 07:16:20 +0000

    update: StickyIntroduceを無効化した

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/provider";
 import theme from "./theme.js";
 import TagsBox from "./TagsBox/TagsBox.tsx";
-import StickyIntroduce from "./StickyIntroduce/StickyIntroduce.tsx";
+// import StickyIntroduce from "./StickyIntroduce/StickyIntroduce.tsx";
 //import remove from "./removeSocialWidgets/removeSocialWidgets.js";
 
 (() => {
@@ -21,23 +21,23 @@ import StickyIntroduce from "./StickyIntroduce/StickyIntroduce.tsx";
     );
   }
 
-  const element02 = document.querySelector(
-    "#article > div.introduce_shoulder_copy"
-    //"#sticky-introduce"
-  );
-  if (element02) {
-    const title = element02.textContent;
-    const formLink = document
-      .querySelector(".rwp-ratings-form-wrap")
-      .getAttribute("id");
-    element02.removeAttribute("class");
-    ReactDOM.render(
-      <ChakraProvider theme={theme}>
-        <StickyIntroduce title={title} formLink={formLink}></StickyIntroduce>
-      </ChakraProvider>,
-      element02
-    );
-  }
+  // const element02 = document.querySelector(
+  //   "#article > div.introduce_shoulder_copy"
+  //   //"#sticky-introduce"
+  // );
+  // if (element02) {
+  //   const title = element02.textContent;
+  //   const formLink = document
+  //     .querySelector(".rwp-ratings-form-wrap")
+  //     .getAttribute("id");
+  //   element02.removeAttribute("class");
+  //   ReactDOM.render(
+  //     <ChakraProvider theme={theme}>
+  //       <StickyIntroduce title={title} formLink={formLink}></StickyIntroduce>
+  //     </ChakraProvider>,
+  //     element02
+  //   );
+  // }
 
   // const budgetSetector = document.querySelector(
   //   //"#gmwd_container_1 > div:nth-child(4) > div > div > div:nth-child(1) > div.col-lg-5.col-md-5.col-sm-12.col-xs-12 > div"
