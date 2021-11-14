2dc6e31f68496caad6563bf67946a4f47c8966b8
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-14 20:20:36 +0000

    update: ダブルクリックで新しいウィンドウが開くようにした

--- wordpress/10Web-Google-Maps/infoWindowOpenEvent.js
+++ wordpress/10Web-Google-Maps/infoWindowOpenEvent.js
@@ -8,16 +8,28 @@ const addInfoWindowOpenEventListener = (
   infoWindowtype,
   _key
 ) => {
-  google.maps.event.addListener(overlay, "click", () => {
+  console.log({
+    overlay,
+    row, // 店舗情報が格納されている
+    overlayWindow,
+    map,
+    openEvent, // click
+    overlayWindows, // length === 38
+    infoWindowtype, // 0
+    _key, // 1
+  });
+  google.maps.event.addListener(overlay, "dblclick", () => {
     if (row.link_url) {
       window.open(row.link_url);
     }
+  });
+  google.maps.event.addListener(overlay, "click", () => {
     if (openEvent == "click") {
       if (overlayWindow && row.enable_info_window == 1) {
         for (var j = 0; j < overlayWindows.length; j++) {
           overlayWindows[j].open(null, null);
         }
-
+        // 情報ウィンドウを表示
         if (overlayWindow && infoWindowtype == 0) {
           overlayWindow.open(map, overlay);
         } else if (infoWindowtype == 1) {
