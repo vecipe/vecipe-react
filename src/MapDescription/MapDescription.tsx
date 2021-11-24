3604b3d0d032bec775e6d1897c20852cb2978495
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-24 11:53:36 +0000

    update: AspectRatioを追加した

--- src/MapDescription/MapDescription.tsx
+++ src/MapDescription/MapDescription.tsx
@@ -6,7 +6,7 @@ import {
   AccordionPanel,
   AccordionIcon,
 } from "@chakra-ui/react";
-import { Box, Flex } from "@chakra-ui/layout";
+import { AspectRatio, Box, Flex } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
 import { useColorMode } from "@chakra-ui/color-mode";
 
@@ -31,19 +31,23 @@ const MapDescription = (): JSX.Element => {
           </h5>
           <AccordionPanel>
             <Flex direction="row">
-              <Image
-                src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/greenPin.gif"
-                alt="green-pin"
-                boxSize="32px"
-              ></Image>
+              <AspectRatio ratio={1} maxWidth="32px">
+                <Image
+                  src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/greenPin.gif"
+                  alt="green-pin"
+                  boxSize="32px"
+                ></Image>
+              </AspectRatio>
               <Box>全ヴィーガン（すべてのメニューがヴィーガン対応）​</Box>
             </Flex>
             <Flex direction="row">
-              <Image
-                src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/orangePin.gif"
-                alt="orange-pin"
-                boxSize="32px"
-              ></Image>
+              <AspectRatio ratio={1} maxWidth="32px">
+                <Image
+                  src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/orangePin.gif"
+                  alt="orange-pin"
+                  boxSize="32px"
+                ></Image>
+              </AspectRatio>
               <Box>一部ヴィーガン（一部のメニューがヴィーガン対応）​</Box>
             </Flex>
           </AccordionPanel>
