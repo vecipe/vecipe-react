75993e5399f0ce490c23766b22b7d1c2f035ea1e
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-24 22:04:58 +0000

    fix: AspectRatioを削除しobject-fitを設定した

--- src/MapDescription/MapDescription.tsx
+++ src/MapDescription/MapDescription.tsx
@@ -31,23 +31,21 @@ const MapDescription = (): JSX.Element => {
           </h5>
           <AccordionPanel>
             <Flex direction="row">
-              <AspectRatio ratio={1} maxWidth="32px">
-                <Image
-                  src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/greenPin.gif"
-                  alt="green-pin"
-                  boxSize="32px"
-                ></Image>
-              </AspectRatio>
+              <Image
+                src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/greenPin.gif"
+                alt="green-pin"
+                boxSize="32px"
+                fit="contain"
+              ></Image>
               <Box>全ヴィーガン（すべてのメニューがヴィーガン対応）​</Box>
             </Flex>
             <Flex direction="row">
-              <AspectRatio ratio={1} maxWidth="32px">
-                <Image
-                  src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/orangePin.gif"
-                  alt="orange-pin"
-                  boxSize="32px"
-                ></Image>
-              </AspectRatio>
+              <Image
+                src="https://linked-by-vecipe.com/blog/wp-content/uploads/2021/11/orangePin.gif"
+                alt="orange-pin"
+                boxSize="32px"
+                fit="contain"
+              ></Image>
               <Box>一部ヴィーガン（一部のメニューがヴィーガン対応）​</Box>
             </Flex>
           </AccordionPanel>
